6635f072d27f0523aef0557d8eb8c7e3ad47fd10
Rogerio Pontes <rogeriopontesti@gmail.com>
2024-10-10 16:59:03 -0300

    Eventos Home Page e Animação testes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,61 @@
 @extends('layouts.default.theme')
 @section("title", env("APP_NAME") . " :: Página Inicial")
 @section('content')
-    <section id="headline">
-        <div class="container my-3">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="text-info">{{ __("Transformando vidas através da inclusão digital e inovação tecnológica") }}</h1>
-                    <h3>{{ __("Participe de uma comunidade que está mudando o futuro através do conhecimento digital") }}</h3>
-                    <a class="btn btn-lg btn-success" href="#">{{ __("Junte-se a nós:") }}</a>
-                </div>
+
+    <section id="headline mb-3">
+        <div class="context">
+            <h1>{{  __("Transformando vidas através da Inclusão Digital e Inovação Tecnológica") }}</h1>
+            <h3 class="text-dark text-center">{{ __("Participe de uma comunidade que está mudando o futuro através do conhecimento digital") }}</h3>
+            <div class="d-flex gap-3 justify-content-center lead fw-normal">
+                <a class="btn btn-lg btn-success d-flex justify-content-center align-items-center" href="#" style="z-index: 10000">
+                    {{ __("Junte-se a nós") }}
+                    <span class="material-symbols-outlined"> chevron_right </span>
+                </a>
+                <a class="btn btn-lg btn-danger d-flex justify-content-center align-items-center" href="#" style="z-index: 10000">
+                Sobre
+                    <span class="material-symbols-outlined"> chevron_right </span>
+                </a>
             </div>
+        <!--            <div class="position-relative overflow-hidden p-3 p-md-5 mb-3 text-center bg-body-tertiary">
+                        <div class="col-md-6 p-lg-5 mx-auto my-5">
+                            
+                        </div>
+                    </div>-->
         </div>
+
+
+        <div class="area" >
+            <ul class="circles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+<!--        <div class="position-relative overflow-hidden p-3 p-md-5 mb-3 text-center bg-body-tertiary" style="background-image: url({{ asset("default/assets/img/fundo-moderno-de-linhas-e-pontos-de-conexao.jpg") }}); background-size: cover; background-repeat: no-repeat; background-blend-mode: multiply;">
+<div class="col-md-6 p-lg-5 mx-auto my-5">
+<h1 class="display-3 fw-bold">{{  __("Transformando vidas através da Inclusão Digital e Inovação Tecnológica") }}</h1>
+<h3 class="fw-normal text-muted mb-3">{{ __("Participe de uma comunidade que está mudando o futuro através do conhecimento digital") }}</h3>
+<div class="d-flex gap-3 justify-content-center lead fw-normal">
+<a class="btn btn-lg btn-success" href="#">
+                        {{ __("Junte-se a nós") }}
+<span class="material-symbols-outlined"> chevron_right </span>
+</a>
+<a class="btn btn-lg btn-danger" href="#">
+Sobre
+<span class="material-symbols-outlined"> chevron_right </span>
+</a>
+</div>
+</div>
+<div class="product-device shadow-sm d-none d-md-block"></div>
+<div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+</div>-->
     </section>
 
     <section>
--- resources/views/layouts/default/theme.blade.php
+++ resources/views/layouts/default/theme.blade.php
@@ -2,12 +2,13 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include('layouts.default.elements.head')   
     <body>
-    <main>
-        @yield('content')
-    </main>
-    <footer>
-        @include('layouts.default.elements.modal')   
-        @include('layouts.default.elements.footer')   
-    </footer>
+    @include('layouts.default.elements.nav')
+<main>
+    @yield('content')
+</main>
+<footer>
+    @include('layouts.default.elements.modal')   
+    @include('layouts.default.elements.footer')   
+</footer>
 </body>
 </html>
